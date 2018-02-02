cc357e4dfdf261d1f994332af455d9cab231fe11
Joshua Bohde <bohdetroit@gmail.com>
2018-02-01 19:06:45 -0500

    location services

--- assets/js/google-maps-api-2.js
+++ assets/js/google-maps-api-2.js
@@ -5,39 +5,19 @@
   var places;
   var infoWindow;
   var pos;
-  var places;
-  var cooperHewitt;
-  var results = [];
+  var directionsService;
 
-      function initMap() {
+    function initMap() {
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var directionsService = new google.maps.DirectionsService;
         var map = new google.maps.Map(document.getElementById('map'), {
-          mapTypeControl: false,
-          center: {lat:40.7844, lng:-73.9582},
-          zoom: 12
+            // mapTypeControl: false,
+            center: {lat:40.7844, lng:-73.9582},
+            zoom: 12
         });
-        var request = {
-            placeId: 'ChIJsT8qSaJYwokR-m20OGJUKCA'
-          };
 
-    
-        function callback(place, status) {
-          if (status == google.maps.places.PlacesServiceStatus.OK) {
-            console.log(place);
-            cooperHewitt = place;
-            console.log(cooperHewitt.geometry.location);
-            var marker = new google.maps.Marker({
-              map: map,
-              place: {
-                placeId: cooperHewitt.place_id,
-                location: cooperHewitt.geometry.location
-              }
-            });
-          }
-        }
-        places = new google.maps.places.PlacesService(map);
-        places.getDetails(request, callback);;
         infoWindow = new google.maps.InfoWindow;
-        new AutocompleteDirectionsHandler(map);
+        
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
@@ -45,103 +25,46 @@
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-
             infoWindow.setPosition(pos);
             infoWindow.setContent('Current location.');
             infoWindow.open(map);
-            map.setCenter(pos);
+            map.setCenter(pos);        
+            function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+                var selectedMode = document.getElementById('mode').value;
+                console.log(pos);
+                directionsService.route({
+                  origin: pos,
+                  destination: {lat:40.7844, lng:-73.9582},  // Ocean Beach.
+                  // Note that Javascript allows us to access the constant
+                  // using square brackets and a string value as its
+                  // "property."
+                  travelMode: google.maps.TravelMode[selectedMode]
+                }, function(response, status) {
+                  if (status == 'OK') {
+                    directionsDisplay.setDirections(response);
+                  } else {
+                    window.alert('Directions request failed due to ' + status);
+                  }
+                });
+            }
+            calculateAndDisplayRoute(directionsService, directionsDisplay);
+            document.getElementById('mode').addEventListener('change', function() {
+            calculateAndDisplayRoute(directionsService, directionsDisplay);
+        });
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
           });
         } else {
-          // Browser doesn't support Geolocation
+        //   // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, map.getCenter());
         }
-        
-      }
+        directionsDisplay.setMap(map);
+    }
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
         infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
         infoWindow.open(map);
-      }
-
-      function AutocompleteDirectionsHandler(map) {
-        this.map = map;
-        this.originPlaceId = null;
-        this.destinationPlaceId = null;
-        this.travelMode = 'WALKING';
-        var originInput = document.getElementById('origin-input');
-        var destinationInput = document.getElementById('destination-input');
-        var modeSelector = document.getElementById('mode-selector');
-        this.directionsService = new google.maps.DirectionsService;
-        this.directionsDisplay = new google.maps.DirectionsRenderer;
-        this.directionsDisplay.setMap(map);
-
-        var originAutocomplete = new google.maps.places.Autocomplete(
-            originInput, {placeIdOnly: true});
-        var destinationAutocomplete = new google.maps.places.Autocomplete(
-            destinationInput, {placeIdOnly: true});
-
-        this.setupClickListener('changemode-walking', 'WALKING');
-        this.setupClickListener('changemode-transit', 'TRANSIT');
-        this.setupClickListener('changemode-driving', 'DRIVING');
-
-        this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
-        this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
-
-        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
-        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
-        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
-      }
-
-      // Sets a listener on a radio button to change the filter type on Places
-      // Autocomplete.
-      AutocompleteDirectionsHandler.prototype.setupClickListener = function(id, mode) {
-        var radioButton = document.getElementById(id);
-        var me = this;
-        radioButton.addEventListener('click', function() {
-          me.travelMode = mode;
-          me.route();
-        });
-      };
-
-      AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(autocomplete, mode) {
-        var me = this;
-        autocomplete.bindTo('bounds', this.map);
-        autocomplete.addListener('place_changed', function() {
-          var place = autocomplete.getPlace();
-          if (!place.place_id) {
-            window.alert("Please select an option from the dropdown list.");
-            return;
-          }
-          if (mode === 'ORIG') {
-            me.originPlaceId = place.place_id;
-          } else {
-            me.destinationPlaceId = place.place_id;
-          }
-          me.route();
-        });
-
-      };
-
-      AutocompleteDirectionsHandler.prototype.route = function() {
-        if (!this.originPlaceId || !this.destinationPlaceId) {
-          return;
-        }
-        var me = this;
-
-        this.directionsService.route({
-          origin: {'placeId': this.originPlaceId},
-          destination: {'placeId': this.destinationPlaceId},
-          travelMode: this.travelMode
-        }, function(response, status) {
-          if (status === 'OK') {
-            me.directionsDisplay.setDirections(response);
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
-      };
+    }
